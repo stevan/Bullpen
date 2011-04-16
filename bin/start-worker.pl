728dea2cad3809852a0b4192e96c56d2edba37b5
Stevan Little <stevan.little@iinteractive.com>
2011-04-15 22:00:18 -0400

    fixing the paths to the jars

--- bin/start-worker.pl
+++ bin/start-worker.pl
@@ -3,7 +3,7 @@
 system(
     'java',
     '-Djava.library.path=/usr/local/lib',
-    '-cp', 'jars/zmq.jar:jars/json_simple.jar:build',
+    '-cp', 'lib/zmq.jar:lib/json_simple.jar:build',
     'com.iinteractive.Bullpen.Runner',
     'Worker',
     $ARGV[0]
