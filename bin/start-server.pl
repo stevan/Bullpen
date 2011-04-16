52f30a4581cc649ee8a50aa21dfec89be4ba0388
Stevan Little <stevan.little@iinteractive.com>
2011-04-15 20:40:51 -0400

    add ant build script

--- bin/start-server.pl
+++ bin/start-server.pl
@@ -3,7 +3,7 @@
 system(
     'java',
     '-Djava.library.path=/usr/local/lib',
-    '-cp', 'jars/zmq.jar:jars/json_simple.jar:lib',
+    '-cp', 'jars/zmq.jar:jars/json_simple.jar:build',
     'com.iinteractive.Bullpen.Runner',
     'Server',
     $ARGV[0]
