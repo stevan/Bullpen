e9285df215138ff6a1b086639f2831b9d02eb97b
Stevan Little <stevan.little@iinteractive.com>
2011-04-20 13:03:22 -0400

    configure can throw an exception

--- src/com/iinteractive/Bullpen/Producer.java
+++ src/com/iinteractive/Bullpen/Producer.java
@@ -4,7 +4,7 @@ package com.iinteractive.Bullpen;
 import java.util.HashMap;
 
 public interface Producer {
-    void    configure   ( HashMap config );
+    void    configure   ( HashMap config ) throws Exception;
     void    initialize  ( String request ) throws Exception;
     void    reset       ();
     boolean hasNext     () throws Exception;
